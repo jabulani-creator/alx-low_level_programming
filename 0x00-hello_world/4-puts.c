f910249ea7678f68fb3dffd8158d3db09dc05582
jabulani-creator <charingabiggie@gmail.com>
2023-06-15 03:38:13 -0700

    fixed trailing lines

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 /**
- * main:Entry point of project
- * Description:  prints a string using the puts function
+ * main - Entry point
+ *
+ * Description: A C program that prints with puts function
+ *
  * Return: Always 0 (Success)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
