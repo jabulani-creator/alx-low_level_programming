30b62794c5d76970d96bd4a11d413ebb3f2e1da7
jabulani-creator <charingabiggie@gmail.com>
2023-06-15 03:24:57 -0700

    betty fix 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include<stdio.h>
-
 /**
- * 
- * Description: A C program that prints with put function
- *
+ * main:  prints "Programming is like building a multilingual puzzle using the puts function
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	
 }	
