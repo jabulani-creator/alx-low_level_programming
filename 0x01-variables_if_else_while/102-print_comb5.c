659e83a0212295c33866ce229a7ffb2ed3580d2f
jabulani-creator <charingabiggie@gmail.com>
2023-06-16 09:37:00 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - print pairs of double digit combos
+ *
+ * Description: print all combinations of two two-digits numbers. 
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number1, number2, number3, number4;
+
+	number1 = 48;
+	while (number1 < 58)
+	{
+		number2 = 48;
+		while (number2 < 58)
+		{
+			number4 = number2 + 1;
+			number3 = number1;
+			while (number3 < 58)
+			{
+				while (number4 < 58)
+				{
+					putchar(number1);
+					putchar(number2);
+					putchar(32);
+					putchar(number3);
+					putchar(number4);
+					if (number1 < 57 || number2  < 56 || number3 < 57 || number4 < 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+					number4++;
+				}
+				number4 = 48;
+				number3++;
+			}
+			number2++;
+		}
+		number1++;
+	}
+	putchar(10);
+	return (0);
+}
