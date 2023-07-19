d4a0f37b578ed0d6b996c5c01f3e81d61cc16753
jabulani-creator <charingabiggie@gmail.com>
2023-07-19 13:01:51 -0700

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+/**
+ * print_name - Entry Point.
+ *
+ * Description:  prints name
+ *
+ * @name: name to print
+ *
+ * @f: function pointer that doesn't return anything
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
